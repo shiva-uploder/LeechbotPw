70cfc750cdda7fc240d2031203bcd965e263360b
shiva-uploder <ps8546572@gmail.com>
2025-04-28 15:25:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 api_id = "26415470"
 api_hash = "b66faafc396e1e5f535f8e02f2d77919"
-bot_token = "7632864743:AAE75Orkszc4jXShWXHFXTJzOitRWY7Tnao"
+bot_token = "7052472056:AAFDjscYXK8KfREHCKelg9FCr4_m7hXKJHY"
 auth_users = [7535138140]
 sudo_users = [7405403151]
